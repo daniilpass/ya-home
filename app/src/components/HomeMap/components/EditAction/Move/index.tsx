8a8fbf5a70713709f19f8ad10c9c2796dc7e5993
Daniil <anil1641@mail.ru>
2024-01-25 21:12:57 +0300

    chore: correct svg tags

--- app/src/components/HomeMap/components/EditAction/Move/index.tsx
+++ app/src/components/HomeMap/components/EditAction/Move/index.tsx
@@ -24,18 +24,18 @@ const EditActionMove: FC<Props> = ({
                 x={x - EDIT_ACTION_SIZE / 2}
                 y={y - EDIT_ACTION_SIZE / 2}
                 fill="none"
-                viewBox="-4.8 -4.8 57.60 57.60"
+                viewBox="0 0 48 48"
             >
                 <g> 
-                    <circle cx="24" cy="24" r="24" fill="white" fill-opacity="0.2"></circle>
-                    <path d="M20 12L24 16L28 12" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M24 16V4" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M20 36L24 32L28 36" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M24 32V44" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M36 20L32 24L36 28" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M32 24L44 24" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M12 20L16 24L12 28" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
-                    <path d="M16 24H4" stroke={strokeColor} stroke-width="4" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <circle cx="24" cy="24" r="24" fill="white" fillOpacity="0.2"></circle>
+                    <path d="M20 12L24 16L28 12" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M24 16V4" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M20 36L24 32L28 36" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M24 32V44" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M36 20L32 24L36 28" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M32 24L44 24" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M12 20L16 24L12 28" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M16 24H4" stroke={strokeColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"></path>
                     <circle cx="24" cy="24" r="2" fill={strokeColor}></circle>
                 </g>
             </svg>
