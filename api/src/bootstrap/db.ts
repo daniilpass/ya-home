89795e1ba6a9751d85b59c20e12280118bd6c139
Daniil <anil1641@mail.ru>
2024-02-02 00:33:39 +0300

    feat: demo data

--- api/src/bootstrap/db.ts
+++ api/src/bootstrap/db.ts
@@ -1,7 +1,24 @@
 import { Sequelize } from 'sequelize';
 
+import { Plan as HomePlan } from '@homemap/shared/index.js';
+
 import { Plan } from '../dal/model/index.js';
 import { planSchema } from '../dal/schema/index.js';
+import planJson from '../demo/plan.json' assert { type: "json" };
+import { DEMO_USER_ID } from '../demo/constants.js';
+
+const createDemoData = async () => {
+    const [_, created] = await Plan.findOrCreate({
+        where: { userId: DEMO_USER_ID},
+        defaults: {
+            userId: DEMO_USER_ID,
+            json: planJson as unknown as HomePlan
+        }
+    });
+    if (created) {
+        console.log("[db] Demo data created");
+    }
+}
 
 export const bootstrapDatabase = async () => {
     console.log("[db] Configure database");
@@ -21,5 +38,7 @@ export const bootstrapDatabase = async () => {
     await sequelize.sync();
     
     console.log("[db] Sync all defined models completed");
+
+    await createDemoData();
 }
 
