dfc5832990e07653f75d29f2c300b006e6f14a05
Daniil <anil1641@mail.ru>
2024-02-01 18:46:26 +0300

    feat: api response as record

--- api/src/controllers/devices.ts
+++ api/src/controllers/devices.ts
@@ -3,17 +3,20 @@ import { NextFunction, Request, Response } from 'express';
 import { Device, DeviceAction, DeviceActionResult } from '@homemap/shared';
 
 import yaclient from '../yaClient/index.js';
-import { mapYaDeviceToDevice } from '../mappers/index.js';
-import { mapDeviceActionToYaDevicesActions } from '../mappers/mapDeviceActionToYaDevicesActions.js';
+import {
+    mapYaDeviceToDevice,
+    mapDeviceActionToYaDevicesActions,
+    mapYaDeviceActionsResultToDeviceActionsResult,
+    mapToRecord,
+} from '../mappers/index.js';
 import { YaDevicesActionsRequest } from '../yaClient/model/requests/YaDevicesActionsRequest.js';
 import { YaUserInfoResponse } from '../yaClient/model/responses/YaUserInfoResponse.js';
 import { YaDevicesActionsResponse } from '../yaClient/model/responses/YaDevicesActionsResponse.js';
-import { mapYaDeviceActionsResultToDeviceActionsResult } from '../mappers/mapYaDeviceActionsResultToDeviceActionsResult.js';
 
 export const getDevices = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const response: YaUserInfoResponse = await yaclient.getUserInfo();
-        const result: Device[] = response.devices.map<Device>(mapYaDeviceToDevice);
+        const result: Record<string, Device> = mapToRecord(response.devices, 'id', mapYaDeviceToDevice);
         res.json(result);
     } catch (error) {
         next(error);
@@ -30,7 +33,11 @@ export const postDevicesActions = async (req: Request, res: Response, next: Next
         };
     
         const response: YaDevicesActionsResponse = await yaclient.postDevicesActions(deviceActionRequest);
-        const result: DeviceActionResult[] = response.devices.map<DeviceActionResult>(mapYaDeviceActionsResultToDeviceActionsResult);
+        const result: Record<string, DeviceActionResult> = mapToRecord(
+            response.devices,
+            'id',
+            mapYaDeviceActionsResultToDeviceActionsResult,
+        );
         res.json(result);       
     } catch (error) {
         next(error);
--- api/src/mappers/index.ts
+++ api/src/mappers/index.ts
@@ -1,2 +1,5 @@
 export { mapYaCapabilitiesToState } from './mapYaCapabilitiesToState.js';
 export { mapYaDeviceToDevice } from './mapYaDeviceToDevice.js';
+export { mapDeviceActionToYaDevicesActions } from './mapDeviceActionToYaDevicesActions.js';
+export { mapYaDeviceActionsResultToDeviceActionsResult } from './mapYaDeviceActionsResultToDeviceActionsResult.js';
+export * from  './utils.js';
