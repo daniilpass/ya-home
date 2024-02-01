bd00492c0977761d60d58ff7aa4d0411817819b9
Daniil <anil1641@mail.ru>
2024-02-01 21:26:17 +0300

    fix: missing imports

--- shared/src/types/home/index.ts
+++ shared/src/types/home/index.ts
@@ -2,3 +2,4 @@ export * from './devices/index.js';
 export * from './deviceTypes/index.js'
 export * from './deviceStates/index.js';
 export * from './deviceAction/index.js';
+export * from './plan/index.js';
--- shared/src/types/home/plan/index.ts
+++ shared/src/types/home/plan/index.ts
@@ -1,2 +1,4 @@
 export type { Plan } from './Plan.js';
 export type { PlanBackground } from './PlanBackground.js';
+export type { PlanDevice } from './PlanDevice.js';
+export type { PlanDeviceArea } from './PlanDeviceArea.js';
