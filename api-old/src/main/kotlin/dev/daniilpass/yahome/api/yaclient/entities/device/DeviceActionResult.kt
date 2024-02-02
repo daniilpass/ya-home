80cf9461f91324cdafc36c8f5740d7ba44d92bf9
Daniil <anil1641@mail.ru>
2024-02-02 23:17:48 +0300

    chore: del old kotlin api

--- api-old/src/main/kotlin/dev/daniilpass/yahome/api/yaclient/entities/device/DeviceActionResult.kt
+++
@@ -1,8 +0,0 @@
-package dev.daniilpass.yahome.api.yaclient.entities.device
-
-import dev.daniilpass.yahome.api.yaclient.entities.Capability
-
-data class DeviceActionResult (
-    val id: String,
-    val capabilities: List<Capability>
-)
--- api-old/src/main/kotlin/dev/daniilpass/yahome/api/yaclient/model/HomeInfoResponse.kt
+++
@@ -1,7 +0,0 @@
-package dev.daniilpass.yahome.api.yaclient.model
-
-import dev.daniilpass.yahome.api.yaclient.entities.device.Device
-
-data class HomeInfoResponse (
-    val devices: List<Device>
-)
