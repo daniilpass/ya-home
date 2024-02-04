e5bc01c3ccd73137190c6df642f153b095ea64e4
Daniil <anil1641@mail.ru>
2024-02-04 19:58:45 +0300

    feat: log constants at startup

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,4 +1,11 @@
+import { logger } from './utils';
+
 export const PORT = process.env.PORT || 3000;
 export const YAPI_LOGIN_BASE_URL =  process.env.YAPI_LOGIN_BASE_URL;
 export const YAPI_IOT_BASE_URL =  process.env.YAPI_IOT_BASE_URL;
 export const YAPI_AUTH_TOKEN =  process.env.YAPI_AUTH_TOKEN;
+
+logger.info('[config] PORT = ' + PORT );
+logger.info('[config] YAPI_LOGIN_BASE_URL = ' + YAPI_LOGIN_BASE_URL);
+logger.info('[config] YAPI_IOT_BASE_URLORT = ' + YAPI_IOT_BASE_URL);
+logger.info('[config] YAPI_AUTH_TOKEN.length = ' + YAPI_AUTH_TOKEN?.length);
