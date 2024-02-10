aab67a894880198b8ee2b7f2ee935642884fdcbb
Daniil <anil1641@mail.ru>
2024-02-10 22:19:00 +0300

    chore: del console log

--- packages/app/src/components/ColorPickerButton/utils.ts
+++ packages/app/src/components/ColorPickerButton/utils.ts
@@ -22,12 +22,10 @@ export const hexToRgb = (color: string) => {
 
 export const getContrastColorRGB = (color: RGBColor): RGBColor => {
     const v = (color.r + color.g + color.b) / 3 > colorThreshold ? 0 : 255;
-    console.log('HELLO', (color.r + color.g + color.b) / 3, color.r + color.g + color.b)
     return { r: v, g: v, b: v };
 }
 
 export const getContrastColorHEX = (color: string): string => {
-    console.log('HELLO', color, hexToRgb(color))
     const contrastColor = getContrastColorRGB(hexToRgb(color));
     return rgbToHex(contrastColor);
 }
