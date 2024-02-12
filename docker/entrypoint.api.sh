7de15a85da6645536c6bdea836da2b4ad8375ffc
Daniil <anil1641@mail.ru>
2024-02-12 22:49:43 +0300

    fix: demo files

--- docker/entrypoint.api.sh
+++ docker/entrypoint.api.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Copy demo data to a mounted location
-cp -a packages/api/src/demo/media packages/api/appData/media
+cp -a packages/api/src/demo/media packages/api/appData
 # Then run the image's CMD
 exec "$@"
