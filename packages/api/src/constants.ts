b2dd2f314505242a0415f3a45bda47b7bbb883cb
Daniil <anil1641@mail.ru>
2024-04-28 14:47:05 +0300

    fix: .env cache ttl

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -4,7 +4,7 @@ export const PORT = process.env.PORT || 3000;
 export const YAPI_LOGIN_BASE_URL =  process.env.YAPI_LOGIN_BASE_URL;
 export const YAPI_IOT_BASE_URL =  process.env.YAPI_IOT_BASE_URL;
 export const YAPI_AUTH_TOKEN =  process.env.YAPI_AUTH_TOKEN;
-export const CACHE_TTL = Number(process.env.CACHE_TTL) || 0;
+export const CACHE_TTL = Number(process.env.CACHE_TTL) || 5;
 export const DB_STORAGE_PATH = './appData';
 export const MEDIA_STORAGE_PATH = './appData/media';
 
