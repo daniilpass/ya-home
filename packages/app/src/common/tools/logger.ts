f3ddc1e1fb1d94bf545ac0b73b234eef22d9ea3a
Daniil <anil1641@mail.ru>
2025-03-01 12:27:03 +0300

    fix: logger message template

--- packages/app/src/common/tools/logger.ts
+++ packages/app/src/common/tools/logger.ts
@@ -6,24 +6,24 @@ export class Logger {
     constructor(public name: string) {}
 
     debug(message: string, ...data: any[]) {
-        this.enabled && console.debug(`${[this.name]} ${message}`, ...data);
+        this.enabled && console.debug(`[${this.name}] ${message}`, ...data);
     }
 
     info(message: string,...data: any[]) {
-        this.enabled && console.info(`${[this.name]} ${message}`, ...data);
+        this.enabled && console.info(`[${this.name}] ${message}`, ...data);
     }
 
     log(message: string,...data: any[]) {
-        this.enabled && console.log(`${[this.name]} ${message}`, ...data);
+        this.enabled && console.log(`[${this.name}] ${message}`, ...data);
     }
 
     warn(message: string,...data: any[]) {
-        this.enabled && console.warn(`${[this.name]} ${message}`, ...data);
+        this.enabled && console.warn(`[${this.name}] ${message}`, ...data);
     }
 
     error(message: string,...data: any[]) {
-        this.enabled && console.error(`${[this.name]} ${message}`, ...data);
+        this.enabled && console.error(`[${this.name}] ${message}`, ...data);
     }
 }
 
-export const loggerInstance = new Logger('DEFAULT');
+export const loggerInstance = new Logger('Default');
