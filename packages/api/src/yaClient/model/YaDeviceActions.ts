d63d6e60ecffe804b64adeee44ac128a4ea72118
Daniil <anil1641@mail.ru>
2025-07-09 20:52:35 +0300

    fix: lint

--- packages/api/src/yaClient/model/YaDeviceActions.ts
+++ packages/api/src/yaClient/model/YaDeviceActions.ts
@@ -1,6 +1,6 @@
 import type { YaDeviceCapability } from './YaDeviceCapability';
 
-export type YaDeviceAction = Omit<YaDeviceCapability, 'state_changed_at'>
+export type YaDeviceAction = Omit<YaDeviceCapability, 'state_changed_at' | 'parameters'>
 
 export type YaDeviceActions = {
     id: string;
